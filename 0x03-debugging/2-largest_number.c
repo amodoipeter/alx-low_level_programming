bf6f0b529b8de5e72ea24f64ec272714860557a3
Amodoi Peter <amodoipeter@gmail.com>
2022-03-18 01:32:11 +0300

    correct to print the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,25 +8,29 @@
 * Return: largest number
 */
 
-int largest_number()
+int main()
 {
 	int largest;
 	double a, b, c;
 
-  printf("Enter three numbers: ");
-  scanf("%lf %lf %lf", &a, &b, &c);
+  printf("Enter the first integer: ");
+  scanf("%lf", &a);
+  printf("Enter the second integer: ");
+  scanf("%lf", &b);
+  printf("Enter the third integer: ");
+  scanf("%lf", &c);
 	if (a >= b && a >= c)
 		{
-    printf("%.2lf is the largest number.", a);
+            printf("%.2lf is the largest number.", a);
 		}
-  else if (b >= a && b >= c)
+    else if (b >= a && b >= c)
 		{
-    printf("%.2lf is the largest number.", b);
+            printf("%.2lf is the largest number.", b);
 		}
 	else
 		{
-    printf("%.2lf is the largest number.", c);
+            printf("%.2lf is the largest number.", c);
 		}
 
- return 0;
+	return 0;
 }
