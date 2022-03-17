8fcb282c6a7648cadb513f66e48c947305b092f7
Amodoi Peter <amodoipeter@gmail.com>
2022-03-18 00:58:58 +0300

    correct to print the largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,25 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-int main()
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+
+int largest_number(int a, int b, int c)
 {
-    int a, b, c, result, max;
-    printf("\nInput the first integer: "); 
-    scanf("%d", &a);
-    printf("\nInput the second integer: ");
-    scanf("%d", &b);
-    printf("\nInput the third integer: ");
-    scanf("%d", &c);
-    result=(a+b+abs(a-b))/2;
-    max=(result+c+abs(result-c))/2;
-    printf("\nMaximum value of three integers: %d", max);
-	printf("\n");
-    return 0;
+	int largest;
+
+	if (a >= b && a >= c)
+		{
+         largest = a;
+		}
+  else if (b >= a && b >= c)
+		{
+            largest = b;
+		}
+	else
+		{
+         largest = c;
+		}
+
+return (largest);
 }
